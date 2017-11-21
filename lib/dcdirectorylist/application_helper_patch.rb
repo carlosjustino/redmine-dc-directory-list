becaeb13bba1dfc3a79c3e86c0b9573ce5619b2c
carlos.justino08@gmail.com <carlos.justino08@gmail.com>
2017-11-21 14:37:15 -0200

    15707 - Criação do plugin

---
+++ lib/dcdirectorylist/application_helper_patch.rb
@@ -0,0 +1,25 @@
+module DCDirectoryList::ApplicationHelperPatch
+  def self.included(base) # :nodoc:
+    base.class_eval do
+
+      def dcdirectorylist_setting(key)
+        if request.params[:dcdirectorylist]
+          session[:dcdirectorylist] = true
+          session[key] = request.params[key]
+        elsif session[:dcdirectorylist]
+          session[key]
+        else
+          Setting.plugin_dc_directory_list[key.to_s]
+        end
+      end
+
+      def dcdirectorylist_setting?(key)
+        dcdirectorylist_setting(key).present?
+      end
+    end
+  end
+end
+
+unless ApplicationHelper.include? dcdirectorylist::ApplicationHelperPatch
+  ApplicationHelper.send(:include, dcdirectorylist::ApplicationHelperPatch)
+end
